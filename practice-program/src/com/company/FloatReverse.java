354d3de17887edcd8e52e0ccb3040f42cdb7e1e6
asashish <asashish147@gmail.com>
2022-04-09 16:19:03 +0530

    reverse a float number

--- practice-program/src/com/company/FloatReverse.java
+++ practice-program/src/com/company/FloatReverse.java
@@ -3,18 +3,17 @@ package com.company;
 public class FloatReverse {
 
     public static void main(String[] args) {
-        FloatReverse fr = new FloatReverse();
-        float aDecimal = 1234.123f;
+        float aDecimal = 123.123f;
         System.out.println(aDecimal);
         int count=0;
         while (aDecimal % 1 != 0){
             aDecimal = aDecimal*10;
             count++;
         }
-        fr.reverse(count, aDecimal);
+        FloatReverse.reverse(count, aDecimal);
     }
 
-    public float reverse(int count, float value){
+    public static float reverse(int count, float value){
         FloatReverse fr1 = new FloatReverse();
         int number = (int) value;
         float reverseNum = 0;
@@ -27,11 +26,11 @@ public class FloatReverse {
             totalLength++;
         }
         System.out.println(reverseNum);
-        fr1.convertIntoDecimal((totalLength-count), reverseNum);
+        FloatReverse.convertIntoDecimal((totalLength-count), reverseNum);
         return reverseNum;
     }
 
-    public float convertIntoDecimal(int count, float number){
+    public static float convertIntoDecimal(int count, float number){
         int divValue = 1;
         while(count != 0){
             divValue = divValue*10;
